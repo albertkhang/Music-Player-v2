b8a2dc6fd53f5438f11cd54e6c674a7ef92dc256
Albert Khang <albertkhang2017@gmail.com>
2020-02-07 11:03:29 +0700

    complete blur cover background

--- app/src/main/java/com/albertkhang/musicplayerv2/activities/FullPlayerActivity.kt
+++ app/src/main/java/com/albertkhang/musicplayerv2/activities/FullPlayerActivity.kt
@@ -3,14 +3,20 @@ package com.albertkhang.musicplayerv2.activities
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.DimenRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.albertkhang.musicplayerv2.R
-import jp.wasabeef.blurry.Blurry
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import jp.wasabeef.glide.transformations.BlurTransformation
 
 class FullPlayerActivity : AppCompatActivity() {
-    private var clFullPlayerActivity: ConstraintLayout? = null
+    private var imgCoverBackground: ImageView? = null
+
+    private val url_cover =
+        "https://static.yeah1.com/uploads/editors/26/2019/11/21/1rDRXaIzn0IBFTphHKhABzyov6rOToLlwJOONJHg.jpeg"
 
     //    Top Icon
     private var flDownArrow: FrameLayout? = null
@@ -25,7 +31,7 @@ class FullPlayerActivity : AppCompatActivity() {
     }
 
     private fun addControl() {
-//        clFullPlayerActivity = findViewById(R.id.clFullPlayerActivity)
+        imgCoverBackground = findViewById(R.id.imgCoverBackground)
 
 //        Top Icon
         flDownArrow = findViewById(R.id.flDownArrow)
@@ -33,7 +39,7 @@ class FullPlayerActivity : AppCompatActivity() {
     }
 
     private fun addEvent() {
-//        makeBackground()
+        makeBackground()
 
         flDownArrow?.setOnClickListener(View.OnClickListener {
             Toast.makeText(this, "Down Arrow", Toast.LENGTH_SHORT).show()
@@ -45,12 +51,14 @@ class FullPlayerActivity : AppCompatActivity() {
     }
 
     private fun makeBackground() {
-//        clFullPlayerActivity?.post {
-//            Blurry.with(this)
-//                .radius(18)
-//                .sampling(7)
-//                .async()
-//                .onto(clFullPlayerActivity)
-//        }
+        val radius=resources.getInteger(R.integer.dimenFullPlayerCoverBackgroundRadius)
+        val sampling=resources.getInteger(R.integer.dimenFullPlayerCoverBackgroundSampling)
+
+        imgCoverBackground?.let {
+            Glide.with(this)
+                .load(url_cover)
+                .apply(RequestOptions.bitmapTransform(BlurTransformation(radius, sampling)))
+                .into(it)
+        }
     }
 }
