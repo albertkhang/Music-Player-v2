267725a32f8b158989633b2d5f5b56b372a486e7
Albert Khang <albertkhang2017@gmail.com>
2020-02-10 17:08:03 +0700

    change mainActivity into recyclerView UI

---
+++ app/src/main/java/com/albertkhang/app/adapters/SongsAdapter.kt
@@ -0,0 +1,23 @@
+package com.albertkhang.app.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+object SongsAdapter : RecyclerView.Adapter<SongsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
